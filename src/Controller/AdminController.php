f50038471226072458e9ea02ec6f018c7ecb5025
Yoel Velasquez Valencia <yoel.velasquez.valencia@gmail.com>
2022-06-11 21:50:56 -0500

    Upgrade admin dashboard add chartjs widget

--- src/Controller/AdminController.php
+++ src/Controller/AdminController.php
@@ -29,6 +29,16 @@ class AdminController extends AbstractController
             ],
         ]);
 
+        $chart->setOptions([
+            'scales' => [
+                'yAxes' => [[
+                    'ticks' => [
+                        'beginAtZero' => true
+                    ]
+                ]],
+            ],
+        ]);
+
         return $this->render('admin/dashboard.html.twig', [
             'chart' => $chart
         ]);
